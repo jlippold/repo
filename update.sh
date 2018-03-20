df2c7f85c50a060dd824aa965b6cd0a528187ae4
jlippold <jlippold@datacation.com>
2018-03-20 13:05:33 -0400

    fixing paths

--- update.sh
+++ update.sh
@@ -10,10 +10,9 @@ echo "--------------------------"
 
 #./dpkg-scanpackages.pl -m ./debs /dev/null >Packages
 dpkg-scanpackages -m ./debs /dev/null >Packages
+sed -i '' 's/.\/debs/https:\/\/jlippold.github.io\/repo\/debs/g' Packages
 rm -f Packages.bz2
 bzip2 -k Packages
-
-sed -i '' 's/.\/debs/https:\/\/jlippold.github.io\/repo\/debs/g' Packages
 
 
 echo "--------------------------"
